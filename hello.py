4ce5a9e9742668fd515916a37d4335e8c3294607
Sept <sszheng3@gmail.com>
2023-05-21 12:30:20 -0400

    test

--- hello.py
+++ hello.py
@@ -5,6 +5,7 @@ import click
 @click.option('--name', prompt='Your name',
               help='The person to greet.')
 def hello(count, name):
+    # cicd
     """Simple program that greets NAME for a total of COUNT times."""
     for x in range(count):
         click.echo(f"Hello {name}!")
